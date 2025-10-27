488b9681eea900ec869afe68422506e32586b577
陽立 三反田 <ari19871976@outlook.jp>
2025-10-27 22:11:27 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,25 +1,24 @@
-﻿// StaticLib.c : スタティック ライブラリ用の関数を定義します。
-//
-
-#include "framework.h"// プロジェクトで共有の項目を載せる
+﻿#include "framework.h"
+#include <stddef.h> // NULL を定義
 
 #include "../include/my_lib.h"
 
-// num個の整数の和を得る
 int sum(int num, int* array)
 {
 	int sum = 0;
-
 	for (int i = 0; i < num; i++)
 	{
 		sum += array[i];
 	}
-
 	return sum;
 }
 
-// num個の整数の平均を得る
 int average(int num, int* array)
 {
-	return 0x7fffffff;// ■未実装
+	if (num <= 0 || array == NULL)
+	{
+		return 0;
+	}
+	int total = sum(num, array);
+	return total / num;
 }
